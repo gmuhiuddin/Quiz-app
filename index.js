94c3fc47ed8f416cf997b62e6e30bdfa68b96f3f
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-10-23 13:43:48 +0500

    Added html, css only

---
+++ index.js
@@ -0,0 +1,13 @@
+const quizes = [
+    {
+      que: 'What is the Capital of Pakistan?',
+      options: ['Karachi', 'Lahore', 'Islamabad', 'Hyderabad'],
+      correct: 'Islamabad'
+    },
+    {
+      que: 'What is the National Sport of Pakistan?',
+      options: ['Cricket', 'Hockey', 'Tennis'],
+      correct: 'Hockey'
+    }
+  ];
+  
